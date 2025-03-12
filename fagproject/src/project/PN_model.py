32638a1ec0f9deb3f13c1f5bd1bf57363d9400f0
Thousig <asger123asger123@gmail.com>
2025-03-12 15:50:59 +0100

    update to PN and symbolic

--- fagproject/src/project/PN_model.py
+++ fagproject/src/project/PN_model.py
@@ -1,18 +1,17 @@
 import torch
 import torch.nn as nn
-
+import sympy as sp
 
 class PolynomialNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.W = nn.Parameter(torch.randn(3, 3)) 
         
-
     def forward(self, x):
         x_exp = torch.cat([x, torch.ones(x.shape[0], 1)], dim=1) 
-        print(f'x_exp shape: {x_exp.shape}, x_exp: {x_exp}, W shape: {self.W.shape}, x.shape: {x.shape}, x: {x}')
-        print(f'x_exp.unsqueeze(1) shape: {x_exp.unsqueeze(1).shape}, x_exp.unsqueeze(1): {x_exp.unsqueeze(1)}')
-        print(f'x_exp.unsqueeze(2) shape: {x_exp.unsqueeze(2).shape}, x_exp.unsqueeze(2): {x_exp.unsqueeze(2)}, x_exp.unsqueeze(1) @ self.W @ x_exp.unsqueeze(2): {(x_exp.unsqueeze(1) @ self.W @ x_exp.unsqueeze(2)).shape}')
-        return torch.sum(x_exp.unsqueeze(1) @ self.W @ x_exp.unsqueeze(2), dim=(1,2))
+        return torch.sum(x_exp.unsqueeze(1) @ self.W @ x_exp.unsqueeze(2), dim=(1,2), keepdim=True).squeeze(-1)
 
+    def symbolic_forward(self, x, y):
+        x_exp = sp.Matrix([x, y, 1])
+        return (x_exp.T * sp.Matrix(self.W.tolist()) * x_exp)[0]
 
