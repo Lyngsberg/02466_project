464623e9b4f1354cce73979df8ec234b7d528843
lyngsberg <Eliasrasmussen12345@gmail.com>
2025-03-19 12:24:56 +0100

    Changed the data scripts such that we can generate differetn dataset sizes

--- fagproject/src/project/data.py
+++ fagproject/src/project/data.py
@@ -43,59 +43,30 @@ def process_and_save_data(X, Y, Z, filename):
     Z_data = torch.tensor(Z, dtype=torch.float32).unsqueeze(1)
     save_data(filename, X_data, Z_data)
 
-def main(num_samples: int, seed: int, x_low: int, x_high: int, y_low:int, y_high: int):
+def main(num_samples: int, seed: int, x_low: int, x_high: int, y_low:int, y_high: int, ran: int):
 
     random.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
+    for i in range(1,ran+1):
+        X_q, Y_q, Z_q = generate_data(num_samples, quadratic_polynomial, i*x_low, i*x_high, i*y_low, i*y_high)
+        X_c, Y_c, Z_c = generate_data(num_samples, cubic_polynomial, i*x_low, i*x_high, i*y_low, i*y_high)
+        X_s, Y_s, Z_s = generate_data(num_samples, smooth_function, i*x_low, i*x_high, i*y_low, i*y_high)
 
-    X_q, Y_q, Z_q = generate_data(num_samples, quadratic_polynomial, x_low, x_high, y_low, y_high)
-    X_c, Y_c, Z_c = generate_data(num_samples, cubic_polynomial, x_low, x_high, y_low, y_high)
-    X_s, Y_s, Z_s = generate_data(num_samples, smooth_function, x_low, x_high, y_low, y_high)
-
-    X_q_n, Y_q_n, Z_q_n = generate_data_with_noise(num_samples, quadratic_polynomial, 0.1, x_low, x_high, y_low, y_high)
-    X_c_n, Y_c_n, Z_c_n = generate_data_with_noise(num_samples, cubic_polynomial, 0.1, x_low, x_high, y_low, y_high)
-    X_s_n, Y_s_n, Z_s_n = generate_data_with_noise(num_samples, smooth_function, 0.1, x_low, x_high, y_low, y_high)
-
-
-
-    # Process and save all datasets
-    process_and_save_data(X_q, Y_q, Z_q, 'fagproject/data/train_q.pkl')
-    process_and_save_data(X_q_n, Y_q_n, Z_q_n, 'fagproject/data/train_q_n.pkl')
-    process_and_save_data(X_c, Y_c, Z_c, 'fagproject/data/train_c.pkl')
-    process_and_save_data(X_c_n, Y_c_n, Z_c_n, 'fagproject/data/train_c_n.pkl')
-    process_and_save_data(X_s, Y_s, Z_s, 'fagproject/data/train_s.pkl')
-    process_and_save_data(X_s_n, Y_s_n,Z_s_n, 'fagproject/data/train_s_n.pkl')
+        X_q_n, Y_q_n, Z_q_n = generate_data_with_noise(num_samples, quadratic_polynomial, 0.1, i*x_low, i*x_high, i*y_low, i*y_high)
+        X_c_n, Y_c_n, Z_c_n = generate_data_with_noise(num_samples, cubic_polynomial, 0.1, i*x_low, i*x_high, i*y_low, i*y_high)
+        X_s_n, Y_s_n, Z_s_n = generate_data_with_noise(num_samples, smooth_function, 0.1, i*x_low, i*x_high, i*y_low, i*y_high)
 
 
-    # Make 6 sub-3d plots for each of the functions
-    fig = plt.figure(figsize=(15, 10))
 
-    ax1 = fig.add_subplot(231, projection='3d')
-    ax1.scatter(X_q, Y_q, Z_q)
-    ax1.set_title('Quadratic Polynomial')
+        # Process and save all datasets
+        process_and_save_data(X_q, Y_q, Z_q, f'fagproject/data/train_q_{i}.pkl')
+        process_and_save_data(X_q_n, Y_q_n, Z_q_n, f'fagproject/data/train_q_n_{i}.pkl')
+        process_and_save_data(X_c, Y_c, Z_c, f'fagproject/data/train_c_{i}.pkl')
+        process_and_save_data(X_c_n, Y_c_n, Z_c_n, f'fagproject/data/train_c_n_{i}.pkl')
+        process_and_save_data(X_s, Y_s, Z_s, f'fagproject/data/train_s_{i}.pkl')
+        process_and_save_data(X_s_n, Y_s_n,Z_s_n, f'fagproject/data/train_s_n_{i}.pkl')
 
-    ax2 = fig.add_subplot(232, projection='3d')
-    ax2.scatter(X_c, Y_c, Z_c)
-    ax2.set_title('Cubic Polynomial')
-
-    ax3 = fig.add_subplot(233, projection='3d')
-    ax3.scatter(X_s, Y_s, Z_s)
-    ax3.set_title('Smooth Function')
-
-    ax4 = fig.add_subplot(234, projection='3d')
-    ax4.scatter(X_q_n, Y_q_n, Z_q_n)
-    ax4.set_title('Quadratic Polynomial with Noise')
-
-    ax5 = fig.add_subplot(235, projection='3d')
-    ax5.scatter(X_c_n, Y_c_n, Z_c_n)
-    ax5.set_title('Cubic Polynomial with Noise')
-
-    ax6 = fig.add_subplot(236, projection='3d')
-    ax6.scatter(X_s_n, Y_s_n, Z_s_n)
-    ax6.set_title('Smooth Function with Noise')
-
-    plt.show()
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -105,6 +76,7 @@ if __name__=="__main__":
     parser.add_argument("--y_low", type=int, default=-1)
     parser.add_argument("--y_high", type=int, default=1)
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--ran", type=int, default=1)
     args = parser.parse_args()
 
-    main(args.num_samples, args.seed, args.x_low, args.x_high, args.y_low, args.y_high)
+    main(args.num_samples, args.seed, args.x_low, args.x_high, args.y_low, args.y_high, args.ran)
