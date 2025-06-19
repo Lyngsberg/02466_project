703591c30bb4f19436bbc0795a018174342b3268
Thousig <asger123asger123@gmail.com>
2025-06-19 10:32:04 +0200

    changes to train_deep

--- fagproject/src/project/Deep_PN/train_deep.py
+++ fagproject/src/project/Deep_PN/train_deep.py
@@ -124,6 +124,14 @@ def train_model(model, X_train, Y_train, X_val, Y_val, n_epochs, learning_rate=0
 # Data loading
 # path = 'fagproject/data/Student_Performance.csv'
 data = pd.read_excel('fagproject/data/Folds5x2_pp.xlsx')
+PN_layers = [3, 3, 3] # Example layers for Polynomial_Network
+NN_layers = [9, 6, 2] # Example layers for General_NN is actually [4, 9, 6, 2, 1] because of the input layer and output layer
+n_epochs = 10000 # Number of epochs for training
+lr = 0.0001 # Learning rate for the optimizer
+optimizer = 'Adam' # Optimizer type, can be 'Adam', 'SGD', or 'LBFGS'
+#l2_lambda = [0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
+l2_lambda = [1e-4]  # Using a single value for simplicity
+index = 0
 
 # data = pd.read_csv(path).dropna()
 # data.replace({"Yes": 1, "No": 0}, inplace=True)
@@ -147,28 +155,25 @@ y_train = torch.tensor(y_train_np, dtype=torch.float32)
 y_test = torch.tensor(y_test_np, dtype=torch.float32)
 
 num_features = X_train.shape[1]
-layers = [3, 3, 3]
-n_epochs = 10000
-lr = 0.0001
-optimizer = 'Adam'
-l2_lambda = [0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
 
-index = find_optimal_lambda(n_epochs=n_epochs, layers=layers, lr=lr, path=path, optimizer_type=optimizer)
-print(f'l2_val: {l2_lambda[index]}')
 
-Polynomial_Net = Polynomial_Network(layers, in_features=num_features)
-NeuralNet = General_NN(layers, in_features=num_features)
+
+#index = find_optimal_lambda(n_epochs=n_epochs, layers=layers, lr=lr, path=path, optimizer_type=optimizer) # Uncomment this line to find the optimal lambda
+# print(f'l2_val: {l2_lambda[index]}')
+
+Polynomial_Net = Polynomial_Network(PN_layers, in_features=num_features)
+NeuralNet = General_NN(NN_layers, in_features=num_features)
 print(f"In-features: {X_train.shape[1]}")
 
 print("\nTraining Neural Network:")
 NeuralNet, train_losses_NN, val_losses_NN = train_model(
     NeuralNet, X_train, y_train, X_test, y_test, n_epochs=n_epochs,
-    learning_rate=lr, path=path, optimizer_type=optimizer, l2_lambda=l2_lambda[index], scaler_y=scaler_y)
+    learning_rate=lr, optimizer_type=optimizer, l2_lambda=l2_lambda[index], scaler_y=scaler_y)
 
-print(f"\nTraining Polynomial_Network (layers={layers}):")
+print(f"\nTraining Polynomial_Network (layers={PN_layers}):")
 poly_network, train_losses_poly_network, val_losses_poly_network = train_model(
     Polynomial_Net, X_train, y_train, X_test, y_test, n_epochs=n_epochs,
-    learning_rate=lr, path=path, optimizer_type=optimizer, l2_lambda=l2_lambda[index], scaler_y=scaler_y)
+    learning_rate=lr, optimizer_type=optimizer, l2_lambda=l2_lambda[index], scaler_y=scaler_y)
 
 with torch.no_grad():
     loss_fn = nn.MSELoss()
@@ -178,16 +183,16 @@ with torch.no_grad():
 test_loss_poly_network = test_loss_poly_network * (scaler_y.scale_[0] ** 2)
 test_loss_NN = test_loss_NN * (scaler_y.scale_[0] ** 2)
 
-print(f"Test Loss (Neural Network): {test_loss_NN.item():.4f}")
-print(f"Test Loss (Polynomial_Network, layers={layers}): {test_loss_poly_network.item():.4f}")
+print(f"Test Loss (Neural Network, layers={NN_layers}): {test_loss_NN.item():.4f}")
+print(f"Test Loss (Polynomial_Network, layers={PN_layers}): {test_loss_poly_network.item():.4f}")
 
 plt.figure(figsize=(10, 5))
-plt.plot(train_losses_NN, label="Neural-Network - Train Loss", linestyle="solid")
-plt.plot(val_losses_NN, label="Neural-Network - Validation Loss", linestyle="dashed")
-plt.plot(train_losses_poly_network, label=f"Polynomial_Network (layers={layers}) - Train Loss - l2_lambda: {l2_lambda[index]}", linestyle="solid")
-plt.plot(val_losses_poly_network, label=f"Polynomial_Network (layers={layers}) - Validation Loss - l2_lambda: {l2_lambda[index]}", linestyle="dashed")
+plt.plot(train_losses_NN, label=f"Neural-Network (layers={NN_layers}) - Train Loss", linestyle="solid")
+plt.plot(val_losses_NN, label=f"Neural-Network (layers={NN_layers}) - Validation Loss", linestyle="dashed")
+plt.plot(train_losses_poly_network, label=f"Polynomial_Network (layers={PN_layers}) - Train Loss - l2_lambda: {l2_lambda[index]}", linestyle="solid")
+plt.plot(val_losses_poly_network, label=f"Polynomial_Network (layers={PN_layers}) - Validation Loss - l2_lambda: {l2_lambda[index]}", linestyle="dashed")
 plt.xlabel("Epochs")
 plt.ylabel("Loss (original scale)")
 plt.legend()
-plt.title(f"Training vs Validation Loss - {optimizer} - {layers}")
+plt.title(f"Training vs Validation Losss\nNeural Network vs Polynomial Network (l2_lambda={l2_lambda[index]})")
 plt.show()
